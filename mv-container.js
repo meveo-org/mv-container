7b8cabe379925413dfcb8c49b2f7fc7431470f19
Tony Alejandro <tonysviews@gmail.com>
2020-03-18 13:55:23 +0800

    add top and position styles to section code cleanup and formatting

--- mv-container.js
+++ mv-container.js
@@ -16,12 +16,12 @@ export class MvContainer extends LitElement {
       :host {
         font-family: var(--font-family, Arial);
         font-size: var(--font-size-m, 10pt);
-        --light-background: var(--mv-container-background, #FFFFFF);
-        --dark-background: var(--mv-container-dark-background, #373E48);
+        --light-background: var(--mv-container-background, #ffffff);
+        --dark-background: var(--mv-container-dark-background, #373e48);
         --light-color: var(--mv-container-light-color, #000000);
-        --dark-color: var(--mv-container-dark-color, #FFFFFF);
+        --dark-color: var(--mv-container-dark-color, #ffffff);
       }
-      
+
       section {
         min-width: var(--mv-container-min-width, 300px);
         max-width: var(--mv-container-max-width, 500px);
@@ -29,17 +29,22 @@ export class MvContainer extends LitElement {
         max-height: var(--mv-container-max-height, auto);
         margin: var(--mv-container-margin, 20px auto);
         padding: var(--mv-container-padding, 20px);
-        border: var(--mv-container-border, 1px solid #BFBFBF);
+        border: var(--mv-container-border, 1px solid #bfbfbf);
+        top: var(--mv-container-top, 0);
+        position: var(--mv-container-position, relative);
         background-color: var(--background-color);
-        box-shadow: var(--mv-container-shadow, 0 0 13px 0 rgba(42, 42, 42, 0.65));
+        box-shadow: var(
+          --mv-container-shadow,
+          0 0 13px 0 rgba(42, 42, 42, 0.65)
+        );
         color: var(--color);
       }
-      
+
       .light {
         background-color: var(--light-background);
         color: var(--light-color);
       }
-      
+
       .dark {
         background-color: var(--dark-background);
         color: var(--dark-color);
@@ -54,9 +59,9 @@ export class MvContainer extends LitElement {
 
   render() {
     return html`
-    <section class="${this.theme}">
-      <slot></slot>
-    </section>
+      <section class="${this.theme}">
+        <slot></slot>
+      </section>
     `;
   }
 }
